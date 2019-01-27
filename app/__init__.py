3b96799192670cfb154bdacb8f75847b7d91a96a
felexkemboi <felokemboi10@gmail.com>
2019-01-27 10:37:58 +0300

    change the init file

--- app/__init__.py
+++ app/__init__.py
@@ -78,19 +78,7 @@ def create_app(config_name):
         db.init_app(app)
         mail.init_app(app)
         # admin.init_app(app)
+    
+    
 
-    """
-    with app.app_context():
-        user_role = Role(name='user')
-        super_user_role = Role(name ='admin')
-        db.session.add(user_role)
-        db.session.add(super_user_role)
-        db.session.commit()
-
-        test_user = user_datastore.create_user(
-        first_name = "Admin",
-        email = "admin",
-        password = "admin",
-        roles = [user_role,super_user_role]
-        )"""
     return app
